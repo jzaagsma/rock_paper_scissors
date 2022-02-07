d22716db5d1be20373f81b45b9845ae2c0cf6c5e
jzaagsma <joshuazaagsma@gmail.com>
2022-02-07 09:02:54 -0800

    commit on 'main'

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
 const userScore = 0;
 const computerScore = 0; 
 const computerSelection = computerPlay();
+const playerSelection = playerPlay();
 
 
 function computerPlay(x){
     let randomNumber = Math.floor((Math.random() *3) +1);
     switch (randomNumber){
         case 1:
-            return 'rock'
+            return 'rock';
         case 2:
-            return 'paper'
+            return 'paper';
         case 3:
-            return 'scissors'
+            return 'scissors';
     }
 }
 
 console.log(computerSelection);
+
+function playerPlay(x){
+    let selection = prompt("Please enter 'Rock', 'Paper', or 'Scissors'");
+    let answer = selection.toLowerCase();
+    return answer;
+}
+
+console.log(playerSelection);
